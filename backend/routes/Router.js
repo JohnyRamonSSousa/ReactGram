96ad5d35dc831676c59c9eb01c0c4282c26d8295
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2026-01-12 18:16:43 -0300

    atualizado projeto

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const router = express()
+const router = express.Router()
 
+router.use("/api/users", require("./UserRoutes")) 
 
 //teste router
 router.get("/", (req, res) =>{
